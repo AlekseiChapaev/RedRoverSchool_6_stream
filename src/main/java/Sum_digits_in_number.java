02a9861e768928a9c92db2dd703dddef3fe09fed
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-03-14 16:39:22 +0100

    add Sum_digits_in_number

--- src/main/java/Sum_digits_in_number.java
+++ src/main/java/Sum_digits_in_number.java
@@ -4,7 +4,9 @@ public class Sum_digits_in_number {
     public static void main(String[] args) {
         int number = 8;
         int sum = IntStream.iterate(1, s -> s <= number , s -> s + 1).sum();
-
         System.out.println(sum);
+
+        int a = 7;
+        IntStream.iterate(a, s-> s * 2).limit(5).forEach(System.out::println);
     }
 }
