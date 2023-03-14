5e490b7e640285d6dcb96c53a5240fbe0977209d
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-03-14 12:37:12 +0100

    add Multiply_table

---
+++ src/main/java/Multiply_table.java
@@ -0,0 +1,10 @@
+public class Multiply_table {
+    public static void main(String[] args) {
+        int number = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(number + " * " + i + " = ");
+            System.out.println( i * number);
+        }
+    }
+}
