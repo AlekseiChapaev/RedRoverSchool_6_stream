21f4cdf180b0bcaab6d90d0cfe3d8edad6e68d07
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-03-22 09:20:24 +0100

    add hw_7_2_task_1

---
+++ src/main/java/hw_7_2_task_1/Cube.java
@@ -0,0 +1,19 @@
+package hw_7_2_task_1;
+
+public class Cube {
+
+    /**
+     * Create a public class called Cube without a constructor which gets one single private integer variable Side,
+     * a getter GetSide() and a setter SetSide(int num) method for this property.
+     */
+
+    int Side;
+
+    public int getSide() {
+        return this.Side;
+    }
+
+    public void setSide(int Side){
+        this.Side = Side;
+    }
+}
